553b6530d7a2930ef40e0ffa3fd32f77cb8310b4
cesarnobre <cesarnobrefilho@gmail.com>
2016-03-01 19:44:44 -0300

    created base project

---
+++ MeusDados/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MeusDados
+//
+//  Created by Usuário Convidado on 01/03/16.
+//  Copyright © 2016 Usuário Convidado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MeusDadosTests/MeusDadosTests.m
@@ -0,0 +1,39 @@
+//
+//  MeusDadosTests.m
+//  MeusDadosTests
+//
+//  Created by Usuário Convidado on 01/03/16.
+//  Copyright © 2016 Usuário Convidado. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MeusDadosTests : XCTestCase
+
+@end
+
+@implementation MeusDadosTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
